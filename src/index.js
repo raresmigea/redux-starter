9d432d22881c5aca872c37134eca498192012c53
raresmigea <raresini@yahoo.com>
2020-10-04 15:24:18 +0300

    added pipe/compose for function composition

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+import { toLower, wrap } from 'lodash';
+import { compose, pipe } from 'lodash/fp';
+
+let input = '      TeXt     ';
+let output = '<div>' + input.trim() + '</div>';
+console.log('output: ', output);
+
+const trim = (str) => str.trim();
+const wrapInDiv = (str) => `<div>${str}</div>`;
+const toLowerCase = (str) => str.toLowerCase();
+const result = wrapInDiv(trim(input));
+console.log('result: ', result);
+
+// or use this function for composing the functions
+const transform = pipe(trim, wrapInDiv, toLowerCase)(input);
+
+console.log('transform: ', transform);
