951eccefa5efa0ad95cbb7a354f52ea27625526f
raresmigea <raresini@yahoo.com>
2020-10-16 19:17:03 +0300

    added new action to remove a bug

--- src/store.js
+++ src/store.js
@@ -9,5 +9,10 @@ store.dispatch({
     description: 'Bug1',
   },
 });
+
+store.dispatch({
+  type: 'bugRemoved',
+  payload: { id: 1 },
+});
 console.log('store ', store.getState());
 export default store;
