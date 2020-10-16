1ddd9488ce28b14c4d2fa8025239ac1a2e5ea5b3
raresmigea <raresini@yahoo.com>
2020-10-16 19:15:07 +0300

    added an action to the store. moved initial code to initials/js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
-import { toLower, wrap } from 'lodash';
-import { pipe } from 'lodash/fp';
-
-let input = '      TeXt    ';
-let output = '<div>' + input.trim() + '</div>';
-console.log('output: ', output);
-
-const trim = (str) => str.trim();
-const wraps = (type, str) => `<${type}>${str}</${type}>`;
-const toLowerCase = (str) => str.toLowerCase();
-
-// or use this function for composing the functions
-const transform = pipe(trim, wraps, toLowerCase);
-
-console.log('transform: ', transform(input));
+import { store } from './store';
--- src/store.js
+++ src/store.js
@@ -3,4 +3,11 @@ import reducer from './reducer';
 
 const store = createStore(reducer);
 
+store.dispatch({
+  type: 'bugAdded',
+  payload: {
+    description: 'Bug1',
+  },
+});
+console.log('store ', store.getState());
 export default store;
