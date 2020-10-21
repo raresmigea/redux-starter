8834c65c843c187a7a0d72b7d6af2102e6cb7646
raresmigea <raresini@yahoo.com>
2020-10-22 00:03:34 +0300

    Transformed action types to arrow functions

--- src/actions.js
+++ src/actions.js
@@ -1,17 +1,13 @@
 import * as actions from './actionTypes';
 
-export function bugAdded(description) {
-  return {
-    type: actions.BUG_ADDED,
-    payload: {
-      description: 'Bug1',
-    },
-  };
-}
+export const bugRemoved = (id) => ({
+  type: actions.BUG_REMOVED,
+  payload: { id: 2 },
+});
 
-export function bugRemoved(id) {
-  return {
-    type: actions.BUG_REMOVED,
-    payload: { id: 2 },
-  };
-}
+export const bugAdded = (description) => ({
+  type: actions.BUG_ADDED,
+  payload: {
+    description: 'Bug1',
+  },
+});
